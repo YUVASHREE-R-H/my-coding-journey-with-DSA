62a630269bc332f487c959f9d4b9ba9e8322d5b7
YUVASHREE R H <131700869+YUVASHREE-R-H@users.noreply.github.com>
2025-08-16 09:12:50 +0530

    Create StudentGrade.java

---
+++ StudentGrade.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class StudentGrade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the mark : ");
+        int mark = sc.nextInt();
+        if(mark < 0 || mark > 100){
+            System.out.println("Invalid mark");
+        }
+        else if(mark >= 90){
+            System.out.println("Grade A");
+        }
+        else if(mark >= 80){
+            System.out.println("Grade B");
+        }
+        else if(mark >=60){
+            System.out.println("Grade C");
+        }
+        else if(mark >=35){
+            System.out.println("Grade D");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
