b24bb120f24e732d09b2ffe281191f0b41d8541a
YUVASHREE R H <131700869+YUVASHREE-R-H@users.noreply.github.com>
2025-08-16 09:24:20 +0530

    Create SizeChart.java

---
+++ SizeChart.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class SizeChart {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size : ");
+        int size = sc.nextInt();
+        switch(size){
+            case 29:
+                System.out.println("Small");
+                break;
+            case 30:
+                System.out.println("Medium");
+                break;
+            case 38:
+                System.out.println("Large");
+                break;
+            case 42:
+                System.out.println("XLarge");
+                break;
+            default:
+                System.out.println("Invalid size");
+        }
+    }
+}
